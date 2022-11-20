eb8f9b2bc7dda92ba8002731ac50100b8e51724c
Jartto <315764765@qq.com>
2022-11-20 21:38:51 +0800

    feat: update log and env_logger

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # show_rust_cli
 Command Line Applications in Rust
+
+## log 打印 | [文档地址](https://docs.rs/env_logger/0.9.0/env_logger/index.html)
+使用说明
+```
+fn user_log() {
+    env_logger::init();
+    trace!("Progress Bar is start!");
+    thread::sleep(Duration::from_secs(1));
+    info!("Time is out!");
+    thread::sleep(Duration::from_secs(1));
+    warn!("oops, nothing implemented!");
+}
+```
+运行命令，需要带上环境变量，否则无任何输出
+```bash
+# Even setting max level to ‘trace’ or DEBUG_LOG=trace
+RUST_LOG=trace cargo run -- main src/main.rs
+```
+效果演示
+![show](#)
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 use anyhow::{Context, Ok, Result};
 use clap::Parser;
+use std::thread;
+use std::time::Duration;
+use log::{info, trace, warn};
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser, Debug)]
 struct CLI {
@@ -10,17 +13,43 @@ struct CLI {
     path: std::path::PathBuf, // PathBuf is like a String but for file system paths that work cross-platform.
 }
 fn main() -> Result<()> {
-    let path = "test.txt";
     let args = CLI::parse();
     println!("{:?} ", args);
+    let path = args.path;
 
-    let contents =
-        std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
+    let contents = std::fs::read_to_string(&path)
+        .with_context(|| format!("could not read file `{:?}`", path))?;
 
     // for line in contents.lines() {
     //     if line.contains(&args.pattern) {
     //         println!("{}", line);
     //     }
     // }
+
+    // progress_func();
+    user_log();
+
     Ok(())
 }
+// 进度条 indicatif 文档地址：https://crates.io/crates/indicatif
+fn progress_func() {
+    let pb = indicatif::ProgressBar::new(100);
+    for i in 0..100 {
+        thread::sleep(Duration::from_millis(5));
+        // pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
+}
+
+// 日志 log 文档地址：https://crates.io/crates/log
+// https://docs.rs/env_logger/0.9.0/env_logger/index.html
+fn user_log() {
+    env_logger::init();
+    trace!("Progress Bar is start!");
+    thread::sleep(Duration::from_secs(1));
+    info!("Time is out!");
+    thread::sleep(Duration::from_secs(1));
+    warn!("oops, nothing implemented!");
+}
+
